238cc45fa45fb341d1222e438f075b45b322f18e
xiaoda <xiaoda2008@126.com>
2019-11-28 20:20:31 +0800

    更新代码更新逻辑

---
+++ dailyUpdateMysqlData.bat
@@ -0,0 +1,4 @@
+d:
+cd D:\Kettle
+Kitchen.bat /file E:\workspace\StrategyLoopbackTest\dailyUpdateMysqlData.kjb /level Basic /logfile E:\workspace\StrategyLoopbackTest\dailyUpdateMysqlData.log
+@pause
